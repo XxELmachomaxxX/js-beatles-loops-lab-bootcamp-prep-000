49199d96a2150280b82733bc50c873bc79e51c2d
max behling <elmachomax123@gmail.com>
2018-03-06 20:58:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,5 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = ['rock you like a hurricane', 'shake it off', 'wake me up inside']
+  for (let emptyArray)
+  return theBeatlesPlay
+}
